7221e308af34e4e9985f139bea6c4463d199456b
Esther Qiu <qiu0057@umn.edu>
2020-11-15 22:32:06 -0600

    HTML layout & Date

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // CODE EXPLAINED channel
+const refresh = document.querySelector("[data-js=refresh]");
+const date = document.querySelector("[data-js=date]");
+const inputField = document.querySelector("[data-js=input-field]");
+const list = document.querySelector("[data-js=list]");
+
+//class names
+const CHECKED = "fa-check-circle";
+const UNCHECKED = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//show Date
+const today = new Date();
+const options = {weekday:"long", month: "short", day: "numeric" };
+date.innerHTML = today.toLocaleDateString("en-US",options);
